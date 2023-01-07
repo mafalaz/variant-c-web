5aed6b2d40ca60646c41045b0e872020b00ca458
mafalaz <lazuardi264@gmail.com>
2023-01-07 19:47:36 +0700

    fix database

--- login/db.php
+++ login/db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("sql6.freemysqlhosting.net","sql6587436","J29pAs6qdm","sql6587436");
+    $con = mysqli_connect("db4free.net","variantc","variantc123","realuser");
     $conS = mysqli_connect("db4free.net", "variant", "variant123", "backupuser");
     // Check connection
     if (mysqli_connect_errno()){
